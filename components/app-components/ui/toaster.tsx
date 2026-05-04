46a331739683dca1cd56dfe6d103730db16da3c9
samassango <samassango@gmail.com>
2026-05-04 23:11:17 +0200

    fixing unresolved imports

--- components/app-components/ui/toaster.tsx
+++ components/app-components/ui/toaster.tsx
@@ -1,5 +1,6 @@
-import { useToast } from "@/hooks/use-toast";
-import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "@/components/ui/toast";
+
+import { useToast } from "@/components/hooks/use-toast";
+import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "@/components/app-components/ui/toast";
 
 export function Toaster() {
   const { toasts } = useToast();
